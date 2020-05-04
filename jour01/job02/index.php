39e145ca79bc914e19f62987996646514ca14163
rodolphe-moriana <rodolphe.moriana@laplateforme.io>
2020-05-04 15:32:11 +0200

    Create index.php

---
+++ jour01/job02/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* Variables */
+
+$str = "Hello LaPlateforme!";
+
+/* Font */
+
+/* Ceci est un premier commentaire */
+
+echo $str;
+
+echo $str; //Ceci est un deuxième commentaire
+
+echo "<br>";
+
+echo $str; #Ceci est un troisième commentaire
+
+?>
