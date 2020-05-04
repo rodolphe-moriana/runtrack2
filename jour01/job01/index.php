6f66ba95507a5e5a6091381ea36b87223ba1f525
rodolphe-moriana <rodolphe.moriana@laplateforme.io>
2020-05-04 15:27:08 +0200

    Create index.php

---
+++ jour01/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Variables */
+
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+$val = 6+4;
+$myBool = false;
+
+/* Font */
+
+echo $str2." ".$str." ".$str3;
+
+echo "<br>";
+
+echo $val;
+
+echo "<br>";
+
+echo $myBool;
+?>
