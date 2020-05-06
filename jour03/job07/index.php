ccd5f179c05aad053b3a0256aaca4fb1ddd69b82
rodolphe-moriana <rodolphe.moriana@laplateforme.io>
2020-05-06 13:36:37 +0200

    job07

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -2,6 +2,14 @@
 
 $str = "Certaines choses changent, et d'autres ne changeront jamais.";
 
+$array=str_split($str);
 
+for($i=0;$i<strlen($str)-2;$i++){
+
+    $j=$i+1;
+    echo $array[$j];
+}
+
+echo $array[0];
 
 ?>
