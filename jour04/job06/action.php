12259f6ae37bd2bad445f24f431f950c8a89dbaa
rodolphe-moriana <rodolphe.moriana@laplateforme.io>
2020-05-07 12:05:35 +0200

    job06

---
+++ jour04/job06/action.php
@@ -0,0 +1,12 @@
+<?php
+
+    if($_GET["nombre"]%2==0){
+        echo "Nombre pair";
+    }
+
+    else{
+        echo "Nombre impair";
+    }
+
+
+?>
---
+++ jour04/job06/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index Test</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="form.css">
+    </head>
+
+    <body>
+        
+        <header>
+
+        </header>
+
+        <main>
+            <form action="action.php" method="GET" id="form-id">
+                <div class="test">
+                    <label for="nombre">Nombre :</label>
+                </div>
+                <div class="test">
+                    <input type="text" name="nombre" placeholder="nombre" required>
+                </div>
+                <div class="test">
+                    <input type="submit" value="Valider" id="button2">
+                </div>
+            </form>
+        </main>
+
+        <footer>
+
+        </footer>
+
+    </body>
+</html>
